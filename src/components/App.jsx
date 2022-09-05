8748422f3ebaede3a0db96514f695360c47e3851
Ivanna Honcharuk <Ivannatsyganjuk@gmail.com>
2022-09-05 02:08:34 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import { Phonebook } from "./Phonebook/Phonebook";
+import { GlobalStyle } from './GlobalStyle';
+import { Section } from './Section/Section';
+import { ContactsList } from './Contacts/Contacts';
+import { Filter } from './Filter/Filter';
+
+
+export class App extends Component {
+ state = {
+  contacts: [],
+  filter: ''
+}
+
+  handleNewContact = newContact => {
+    if (!this.hasDuplicates(newContact.name)) {
+      this.setState(prevState => {
+        return {
+          contacts: [...prevState.contacts, newContact],
+        };
+      });
+    } else {
+      alert(`${newContact.name} is already in contacts.`);
+    }
+  };
+
+  hasDuplicates(duplicate) {
+    return this.state.contacts.find(({ name }) => name === duplicate);
+  }
+
+   getFilteredContacts = () => {
+    const filterValue = this.state.filter.toLowerCase();
+
+    return this.state.contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filterValue)
+    );
+  };
+
+  onInputChange = evt => {
+    this.setState({ [evt.target.name]: evt.target.value });
+  };
+
+  deleteContact = contactIdToDelete => {
+    this.setState({
+      contacts: this.state.contacts.filter(
+        ({ id }) => id !== contactIdToDelete
+      ),
+    });
+  };
+
+  render() {
+    const filteredContacts = this.getFilteredContacts();
+
+    return (
+      <>
+        <GlobalStyle />
+        <Section title="Phonebook">
+          <Phonebook handleNewContact={this.handleNewContact} />
+        </Section>
+        <Section title="Contacts">
+          <Filter onInputChange={this.onInputChange} />
+          <ContactsList
+            contacts={filteredContacts}
+            deleteContact={this.deleteContact}
+          />
+        </Section>
+      </>
+    );
+  }
 };
